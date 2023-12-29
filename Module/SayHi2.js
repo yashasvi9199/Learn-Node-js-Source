2f799dc2337651855fd81304ac3a6d45991c4552
Yash Haldiya <yashasvi9199@gmail.com>
2023-12-29 15:19:04 +0530

    renamed files in project

--- Module/SayHi2.js
+++ Module/SayHi2.js
@@ -2,12 +2,12 @@
 // syntax : require('<file path>')
 //here we will be storing this in a variable
 
-const names = require('./02_Data')
+const names = require('./Data')
 //or we can deconstruct the values as : 
 // const {john, peter} = require('./-2_Data)
 
-const sayHi = require('./02_Module')
-const data = require('./02_Alternative_Data')
+const sayHi = require('./Module')
+const data = require('./Alternative_Data')
 
 // console.log(names)
 sayHi(names.name1)
---
+++ invokeModule.js
@@ -0,0 +1,1 @@
+require('./invokeModule_module')
