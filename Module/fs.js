c78c68f4d9adf44e243e492f49fdb0932f3d973d
Yash Haldiya <yashasvi9199@gmail.com>
2023-12-29 15:33:46 +0530

    core module : fs

---
+++ Module/fs.js
@@ -0,0 +1,6 @@
+// importing a core non-global module
+const files = require('fs')
+
+// write files synchronously
+// Syntax : fs.writeFileSync(<file directory>,<string>,<encoding>)     note that if file does not exists, then it will be created but the directory should be present
+files.writeFileSync('./fs_files/test.txt', 'Hello, this is my first text file', 'utf8')
