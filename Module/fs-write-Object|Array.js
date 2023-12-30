487795798ac1b8a608430ca5d4fbd22022d811ef
Yash Haldiya <yashasvi9199@gmail.com>
2023-12-30 21:31:32 +0530

    Print/Write array or objects in a files using FS

---
+++ Module/fs-write-Object|Array.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+const dirPath = `${__dirname}/fs_files`
+const dataObject = {
+    orange: 'This is an orange',
+    apple: 'This is an apple',
+    grape: 'This is a grape'
+}
+
+const dataArray = ['orange','apple','grape']
+
+for(let x in dataObject){
+    fs.writeFileSync(`${dirPath}/${x}.txt`,dataObject[x])
+}
+
+dataArray.forEach((name) => {
+    fs.appendFileSync(`${dirPath}/Fruits.txt`,`${name} \n`)
+})
